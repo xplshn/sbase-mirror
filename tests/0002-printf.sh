d77328ca4d1a7aab1905a85faea8bacdcb0cb293
Santtu Lakkala <inz@inz.fi>
2025-11-12 17:49:54 +0200

    printf: Fix multiple flags and read overflow
    Support having multiple flags for a single conversion; at least '+'/'#' and
    '0' are not exclusive.
    
    Further use strspn() instead of inlined version for correct handling of
    string ending with allowed characters.

---
+++ tests/0002-printf.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+
+exp1=exp1.$$
+exp2=exp2.$$
+res1=res1.$$
+res2=res2.$$
+
+cleanup()
+{
+	st=$?
+	rm -f $exp1 $exp2 $res1 $res2
+	exit $st
+}
+
+trap cleanup EXIT HUP INT TERM
+
+cat <<'EOF' > $exp1
+123
+0
+foo
+bar
++001   +2 +003 -400 
+Expected failure
+EOF
+
+cat <<'EOF' > $exp2
+../printf: Missing format specifier.
+EOF
+
+(
+	../printf '123\n'
+	../printf '%d\n'
+	../printf '%b' 'foo\nbar\n'
+
+	# Two flags used simulatenously, + and 0
+	../printf '%+04d %+4d ' 1 2 3 -400; ../printf "\n"
+	# Missing format specifier; should have sane error message
+	../printf '%000' FOO || echo "Expected failure"
+) > $res1 2> $res2
+
+diff -u $exp1 $res1
+diff -u $exp2 $res2
