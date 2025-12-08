fbc4b4252455a126c552e6b4b35adb643ff1135c
Roberto E. Vargas Caballero <k0ga@shike2.net>
2025-12-08 18:20:07 +0100

    tests: Fix 0007-ed.sh

--- tests/0007-ed.sh
+++ tests/0007-ed.sh
@@ -8,6 +8,6 @@ trap 'rm -f $tmp' EXIT
 trap 'rm -f $tmp; kill -KILL $$' HUP INT TERM
 
 printf 'something important' > $tmp
-ed $tmp <<EOF 2>/dev/null | grep something | diff -w $tmp -
+../ed -s $tmp <<EOF 2>/dev/null | diff -w $tmp -
 1p
 EOF
