40a4999b60355a8e498f1d228469aeca17f9a698
Roberto E. Vargas Caballero <k0ga@shike2.net>
2026-01-19 16:25:04 +0100

    tests: Minor modifications and fixes

--- tests/0045-dc.sh
+++ tests/0045-dc.sh
@@ -9,7 +9,7 @@ trap 'exit $?' HUP INT TERM
 
 echo 0 > $tmp
 
-../dc -i <<'EOF' | diff -u - $tmp
+$EXEC ../dc -i <<'EOF' | diff -u - $tmp
 [ 0 Lxs. 2Q]s<128>
 [ .7853981633974483096156608458198757210492923498437764 1/ Lxs. 3Q]s<130>
 [K 52><130> ]s<129>
